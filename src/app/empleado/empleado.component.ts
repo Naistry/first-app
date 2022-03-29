928ccda05a2cecd85267defaa9b255eeb345c8b6
Martin Pastorino <martinpastorino013@gmail.com>
2022-03-29 19:57:00 -0300

    bindings  interpolation

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
+  
+  nombre = 'Martín';
+  apellido = 'Pastorino';
+  public edad = 29;
+  text = "Usuario no registrado";
+  empresa = "Google";
   constructor() { }
 
+  public getEdad(){
+    return this.edad;
+  }
+
+  public setEdad(edad: number){
+    this.edad= edad;
+  }
+
+  habilitarText=false;
+  
+  loginCheckProperty= false;
+
+
+  /*getRegistroUsario (){
+    this.loginCheckProperty = false;
+  }*/
+
+  setUsario(event:Event){
+    
+   // this.text= "Usuario registrado";
+    if((<HTMLInputElement>event.target).value == 'si'){
+      this.text="Se registro";
+    }else {
+      this.text = "No se registro";
+    }
+  }
+
+  
   ngOnInit(): void {
   }
+
 
 }
